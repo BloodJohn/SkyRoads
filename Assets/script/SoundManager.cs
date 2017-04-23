8f3340329596fcc6dc8e979d5e70cb7a442e97c6
Eugene Pavlov <bjohn.ext@gmail.com>
2017-04-23 13:36:37 +0300

    add music

---
+++ Assets/script/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    public bool IsSound;
+
+    [SerializeField]
+    private GameObject Music;
+
+    private static readonly string SoundKey = "muteSound";
+
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+        Application.targetFrameRate = 10;
+
+        IsSound = PlayerPrefs.GetInt(SoundKey, 100) > 0;
+        Music.SetActive(IsSound);
+    }
+
+    public void MuteSound()
+    {
+        if (!IsSound)
+        {
+            IsSound = true;
+            PlayMusic();
+        }
+        else
+        {
+            IsSound = false;
+            StopSound();
+        }
+
+        PlayerPrefs.SetInt(SoundKey, IsSound ? 100 : 0);
+    }
+
+    public void StopSound()
+    {
+        Music.SetActive(false);
+    }
+
+    public void PlayMusic()
+    {
+        if (!IsSound) return;
+        Music.SetActive(true);
+    }
+}
